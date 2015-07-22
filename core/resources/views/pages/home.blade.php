15e45081c5bc2dbeebcebd846f16a34b02548427
Derek Nutile <dereknutile@gmail.com>
2015-07-22 14:47:25 -0700

    Adds a dummy logo.

--- core/resources/views/pages/home.blade.php
+++ core/resources/views/pages/home.blade.php
@@ -1,1 +1,14 @@
-<h1>{{ $title }}</h1>
+@extends('_layouts/landing')
+
+@section('content')
+<section class="copy-section splash-page">
+  <div class="container">
+
+    <div class="col-md-12">
+      <h1 class="page-title text-center">{{ Config::get('app.site_description') }}</h1>
+      <h2 class="page-subtitle text-center">Coming May 2015</h2>
+    </div><!-- /.col -->
+
+  </div><!-- /.half-container -->
+</section><!-- /.copy-section -->
+@stop
