3555d0cb2778e8f62e290808e0d20f4463acf3d4
Derek Nutile <dereknutile@gmail.com>
2015-10-30 09:35:52 -0700

    Fine tunes the authentication views.

--- core/resources/views/auth/login.blade.php
+++ core/resources/views/auth/login.blade.php
@@ -1,52 +1,52 @@
 @extends('_layouts/app')
 
 @section('content')
-<div class="container-fluid">
+<div class="container">
 	<div class="row">
 		<div class="sign-in-form">
 			<div class="well">
-				<h3>Login</h3>
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+				<h3>Sign-In</h3>
+				@if (count($errors) > 0)
+					<div class="alert alert-danger">
+						<strong>Whoops!</strong> There were some problems with your input.<br><br>
+						<ul>
+							@foreach ($errors->all() as $error)
+								<li>{{ $error }}</li>
+							@endforeach
+						</ul>
+					</div>
+				@endif
 
-					<form class="form" role="form" method="POST" action="{{ url('/auth/login') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+				<form class="form" role="form" method="POST" action="{{ url('/auth/login') }}">
+					<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-						<div class="form-group">
-							<label class="control-label">E-Mail Address</label>
-    					<p class="help-block">Use	<em>admin@localhost.com</em> during testing.</p>
-							<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-						</div>
+					<div class="form-group">
+						<label class="control-label">Email Address</label>
+  					<p class="help-block">Use	<em>admin@localhost.com</em> during testing.</p>
+						<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+					</div>
 
-						<div class="form-group">
-							<label class="control-label">Password</label>
-    					<p class="help-block">Use	<em>password</em> during testing.</p>
-							<input type="password" class="form-control" name="password">
-						</div>
+					<div class="form-group">
+						<label class="control-label">Password</label>
+  					<p class="help-block">Use	<em>password</em> during testing.</p>
+						<input type="password" class="form-control" name="password">
+					</div>
 
-						<div class="form-group">
-							<div class="checkbox">
-								<label>
-									<input type="checkbox" name="remember"> Remember Me
-								</label>
-							</div>
+					<div class="form-group">
+						<div class="checkbox">
+							<label>
+								<input type="checkbox" name="remember"> Remember Me
+							</label>
 						</div>
+					</div>
 
-						<div class="form-group">
-						<button type="submit" class="btn btn-primary btn-block">Login</button>
-						<p class="subtle"><a class="btn btn-link" href="{{ url('/password/email') }}"><i class="fa fa-info-circle"></i>&nbsp;Did you forget your password?</a></p>
-						</div>
-					</form>
-			</div>
-		</div>
-	</div>
-</div>
+					<div class="form-group">
+					<button type="submit" class="btn btn-primary btn-block">Login</button>
+					<p class="subtle"><a class="btn btn-link" href="{{ url('/password/email') }}"><i class="fa fa-info-circle"></i>&nbsp;Did you forget your password?</a></p>
+					</div>
+				</form>
+			</div><!-- /.well -->
+		</div><!-- /.sign-in-form -->
+	</div><!-- /.row -->
+</div><!-- /.container -->
 @endsection
--- core/resources/views/auth/password.blade.php
+++ core/resources/views/auth/password.blade.php
@@ -1,50 +1,44 @@
 @extends('_layouts/app')
 
 @section('content')
-<div class="container-fluid">
+<div class="container">
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Reset Password</div>
-				<div class="panel-body">
-					@if (session('status'))
-						<div class="alert alert-success">
-							{{ session('status') }}
-						</div>
-					@endif
+		<div class="sign-in-form">
+			<div class="well">
+				<h3>Reset Password</h3>
+				@if (session('status'))
+					<div class="alert alert-success">
+						{{ session('status') }}
+					</div>
+				@endif
 
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+				@if (count($errors) > 0)
+					<div class="alert alert-danger">
+						<strong>Whoops!</strong> There were some problems with your input.<br><br>
+						<ul>
+							@foreach ($errors->all() as $error)
+								<li>{{ $error }}</li>
+							@endforeach
+						</ul>
+					</div>
+				@endif
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+				<form class="form" role="form" method="POST" action="{{ url('/password/email') }}">
+					<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
+					<div class="form-group">
+						<label class="control-label">Email Address</label>
+						<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+					</div>
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Send Password Reset Link
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+					<div class="form-group">
+						<button type="submit" class="btn btn-primary btn-block">
+							Send Password Reset Link
+						</button>
+					</div>
+				</form>
+			</div><!-- /.well -->
+		</div><!-- /.sign-in-form -->
+	</div><!-- /.row -->
+</div><!-- /.container -->
 @endsection
