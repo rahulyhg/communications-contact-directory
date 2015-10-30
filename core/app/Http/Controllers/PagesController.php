53ddad8470c87a82923b7988168cfd8c08292aab
Derek Nutile <dereknutile@gmail.com>
2015-10-30 09:23:21 -0700

    Fixes a small bug if  doesn't exist.

--- core/app/Http/Controllers/PagesController.php
+++ core/app/Http/Controllers/PagesController.php
@@ -9,16 +9,17 @@ use App\Http\Controllers\Controller;
 
 class PagesController extends Controller
 {
-    public function home()
-    {
-      $title = "Home Page";
-      $page_active = "home";
-      return view('pages.home', compact('title','page_active'));
-    }
-    public function table()
-    {
-      $title = "Table Page";
-      $page_active = "table";
-      return view('pages.table', compact('title','page_active'));
-    }
+  // homepage
+  public function home() {
+    $title = "Home Page";
+    $page_active = "home";
+    return view('pages.home', compact('title','page_active'));
+  }
+
+  // page that displays the table output
+  public function table() {
+    $title = "Table Page";
+    $page_active = "table";
+    return view('pages.table', compact('title','page_active'));
+  }
 }
