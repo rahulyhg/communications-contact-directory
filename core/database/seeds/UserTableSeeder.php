faf685e8655e636c78092e15353806a71bc2d0be
Derek Nutile <dereknutile@gmail.com>
2015-11-01 09:52:30 -0800

    Adds user and directory seeder, or placeholders.  Calls both seeders from the databaseseeder.

---
+++ core/database/seeds/UserTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    // truncate the users table
+    DB::table('users')->truncate();
+
+    // create the test admin user
+    DB::table('users')->insert([
+      'first_name' => 'Admin',
+      'last_name' => 'User',
+      'admin' => 1,
+      'email' => 'admin@localhost.com',
+      'password' => bcrypt('password'),
+    ]);
+  }
+}
