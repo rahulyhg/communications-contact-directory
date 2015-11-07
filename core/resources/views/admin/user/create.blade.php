04475e12b63c945a2afdf12e0f75685c503a6141
Derek Nutile <dereknutile@gmail.com>
2015-11-06 16:44:41 -0800

    Adds functionality to user forms.

--- core/resources/views/admin/user/create.blade.php
+++ core/resources/views/admin/user/create.blade.php
@@ -5,41 +5,43 @@
   <div class="row">
     <div class="content">
 
-    <h1 class="page-title">{{ $title }}</h1>
-    <div class="well">
-    {!! Form::open( ['route' => 'store_users'] ) !!}
-    <div class="form-group">
-      {!! Form::label('first_name', 'First Name') !!}
-      {!! Form::text('first_name', null, ['required', 'class'=>'form-control', 'placeholder'=>'Firstname']) !!}
-    </div>
+      <h1 class="page-title">{{ $title }}</h1>
+      <div class="well">
 
-    <div class="form-group">
-      {!! Form::label('last_name', 'Last Name') !!}
-      {!! Form::text('last_name', null, ['required', 'class'=>'form-control', 'placeholder'=>'Lastname']) !!}
-    </div>
+        {!! Form::open( ['route' => 'store_users'] ) !!}
+        <div class="form-group">
+          {!! Form::label('first_name', 'First Name') !!}
+          {!! Form::text('first_name', null, ['required', 'class'=>'form-control', 'placeholder'=>'Firstname']) !!}
+        </div>
 
-    <div class="form-group">
-      {!! Form::label('email', 'Email') !!}
-      {!! Form::text('email', null, ['required', 'class'=>'form-control', 'placeholder'=>'email@domain.com']) !!}
-    </div>
+        <div class="form-group">
+          {!! Form::label('last_name', 'Last Name') !!}
+          {!! Form::text('last_name', null, ['required', 'class'=>'form-control', 'placeholder'=>'Lastname']) !!}
+        </div>
 
-    <div class="form-group">
-      {!! Form::label('admin', 'Is user an Admin?') !!}
-      {!! Form::checkbox('admin', 'admin', false) !!}
-    </div>
+        <div class="form-group">
+          {!! Form::label('email', 'Email') !!}
+          {!! Form::text('email', null, ['required', 'class'=>'form-control', 'placeholder'=>'email@domain.com']) !!}
+        </div>
 
-    <div class="form-group">
-      {!! Form::label('note', 'Administrative Note') !!}
-      <span class="help-block">Note, the administrative note is not displayed to users.</span>
-      {!! Form::textarea('note', null, ['class'=>'form-control', 'placeholder'=>'Administrative notes go here.']) !!}
-    </div>
+        <div class="form-group">
+          {!! Form::label('admin', 'Is user an Admin?') !!}
+          {!! Form::checkbox('admin', 'admin', false) !!}
+        </div>
 
-    <div class="form-group">
-      {!! Form::submit('Create User', ['class'=>'btn btn-primary form-control']) !!}
-    </div>
-    {!! Form::close() !!}
-    </div>
+        <div class="form-group">
+          {!! Form::label('note', 'Administrative Note') !!}
+          <span class="help-block">Note, the administrative note is not displayed to users.</span>
+          {!! Form::textarea('note', null, ['class'=>'form-control', 'placeholder'=>'Administrative notes go here.']) !!}
+        </div>
 
+        <div class="form-group">
+          {!! Form::submit('Submit', ['class'=>'btn btn-primary']) !!}
+          <a href="{{ route('store_users') }}" class="btn btn-default">Cancel</a>
+        </div>
+        {!! Form::close() !!}
+
+      </div><!-- /.well -->
     </div><!-- /.content -->
   </div><!-- /.row -->
 </div><!-- /.container -->
--- core/resources/views/admin/user/edit.blade.php
+++ core/resources/views/admin/user/edit.blade.php
@@ -5,11 +5,43 @@
   <div class="row">
     <div class="content">
 
-    <h1 class="page-title">{{ $title }}</h1>
-    {!! Form::open() !!}
-    <p>{{ $user->first_name }}</p>
-    {!! Form::close() !!}
+      <h1 class="page-title">{{ $title }}</h1>
+      <div class="well">
 
+        {!! Form::open() !!}
+        <div class="form-group">
+          {!! Form::label('first_name', 'First Name') !!}
+          {!! Form::text('first_name', $user->first_name, ['required', 'class'=>'form-control', 'placeholder'=>'Firstname']) !!}
+        </div>
+
+        <div class="form-group">
+          {!! Form::label('last_name', 'Last Name') !!}
+          {!! Form::text('last_name', $user->last_name, ['required', 'class'=>'form-control', 'placeholder'=>'Lastname']) !!}
+        </div>
+
+        <div class="form-group">
+          {!! Form::label('email', 'Email') !!}
+          {!! Form::text('email', $user->email, ['required', 'class'=>'form-control', 'placeholder'=>'email@domain.com']) !!}
+        </div>
+
+        <div class="form-group">
+          {!! Form::label('admin', 'Is user an Admin?') !!}
+          {!! Form::checkbox('admin', 'admin', $user->admin) !!}
+        </div>
+
+        <div class="form-group">
+          {!! Form::label('note', 'Administrative Note') !!}
+          <span class="help-block">Note, the administrative note is not displayed to users.</span>
+          {!! Form::textarea('note', $user->note, ['class'=>'form-control', 'placeholder'=>'Administrative notes go here.']) !!}
+        </div>
+
+        <div class="form-group">
+          {!! Form::submit('Submit', ['class'=>'btn btn-primary']) !!}
+          <a href="{{ route('store_users') }}" class="btn btn-default">Cancel</a>
+        </div>
+        {!! Form::close() !!}
+
+      </div><!-- /.well -->
     </div><!-- /.content -->
   </div><!-- /.row -->
 </div><!-- /.container -->
