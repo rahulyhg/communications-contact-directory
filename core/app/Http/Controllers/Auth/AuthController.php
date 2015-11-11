c1fc02415ee956b7e82bb739814cbd3ffd862791
Derek Nutile <dereknutile@gmail.com>
2015-11-10 16:54:33 -0800

    Customizes auth controller to use a custom redirect after login.

--- core/app/Http/Controllers/Auth/AuthController.php
+++ core/app/Http/Controllers/Auth/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
     */
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
-    protected $redirectTo = '/';
+    protected $redirectTo = '/admin';
 
     /**
      * Create a new authentication controller instance.
