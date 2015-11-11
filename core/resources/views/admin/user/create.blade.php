d362485ae0041e0427abb0a1f60ad5aee03e9f9a
Derek Nutile <dereknutile@gmail.com>
2015-11-11 07:52:44 -0800

    Removes container wrapper from subview.

--- core/resources/views/admin/user/create.blade.php
+++ core/resources/views/admin/user/create.blade.php
@@ -1,48 +1,44 @@
 @extends('_layouts/admin')
 
 @section('content')
-<div class="container">
-  <div class="row">
-    <div class="content">
+<div class="content">
 
-      <h1 class="page-title">{{ $title }}</h1>
-      <div class="well">
+  <h1 class="page-title">{{ $title }}</h1>
+  <div class="well">
 
-        {!! Form::open( ['route' => 'store_users'] ) !!}
-        <div class="form-group">
-          {!! Form::label('first_name', 'First Name') !!}
-          {!! Form::text('first_name', null, ['required', 'class'=>'form-control', 'placeholder'=>'Firstname']) !!}
-        </div>
+    {!! Form::open( ['route' => 'store_users'] ) !!}
+    <div class="form-group">
+      {!! Form::label('first_name', 'First Name') !!}
+      {!! Form::text('first_name', null, ['required', 'class'=>'form-control', 'placeholder'=>'Firstname']) !!}
+    </div>
 
-        <div class="form-group">
-          {!! Form::label('last_name', 'Last Name') !!}
-          {!! Form::text('last_name', null, ['required', 'class'=>'form-control', 'placeholder'=>'Lastname']) !!}
-        </div>
+    <div class="form-group">
+      {!! Form::label('last_name', 'Last Name') !!}
+      {!! Form::text('last_name', null, ['required', 'class'=>'form-control', 'placeholder'=>'Lastname']) !!}
+    </div>
 
-        <div class="form-group">
-          {!! Form::label('email', 'Email') !!}
-          {!! Form::text('email', null, ['required', 'class'=>'form-control', 'placeholder'=>'email@domain.com']) !!}
-        </div>
+    <div class="form-group">
+      {!! Form::label('email', 'Email') !!}
+      {!! Form::text('email', null, ['required', 'class'=>'form-control', 'placeholder'=>'email@domain.com']) !!}
+    </div>
 
-        <div class="form-group">
-          {!! Form::label('admin', 'Is user an Admin?') !!}
-          {!! Form::checkbox('admin', 'admin', false) !!}
-        </div>
+    <div class="form-group">
+      {!! Form::label('admin', 'Is user an Admin?') !!}
+      {!! Form::checkbox('admin', 'admin', false) !!}
+    </div>
 
-        <div class="form-group">
-          {!! Form::label('note', 'Administrative Note') !!}
-          <span class="help-block">Note, the administrative note is not displayed to users.</span>
-          {!! Form::textarea('note', null, ['class'=>'form-control', 'placeholder'=>'Administrative notes go here.']) !!}
-        </div>
+    <div class="form-group">
+      {!! Form::label('note', 'Administrative Note') !!}
+      <span class="help-block">Note, the administrative note is not displayed to users.</span>
+      {!! Form::textarea('note', null, ['class'=>'form-control', 'placeholder'=>'Administrative notes go here.']) !!}
+    </div>
 
-        <div class="form-group">
-          {!! Form::submit('Submit', ['class'=>'btn btn-primary']) !!}
-          <a href="{{ route('store_users') }}" class="btn btn-default">Cancel</a>
-        </div>
-        {!! Form::close() !!}
+    <div class="form-group">
+      {!! Form::submit('Submit', ['class'=>'btn btn-primary']) !!}
+      <a href="{{ route('store_users') }}" class="btn btn-default">Cancel</a>
+    </div>
+    {!! Form::close() !!}
 
-      </div><!-- /.well -->
-    </div><!-- /.content -->
-  </div><!-- /.row -->
-</div><!-- /.container -->
+  </div><!-- /.well -->
+</div><!-- /.content -->
 @stop
--- core/resources/views/admin/user/index.blade.php
+++ core/resources/views/admin/user/index.blade.php
@@ -1,46 +1,41 @@
 @extends('_layouts/admin')
 
 @section('content')
-<div class="container">
 
-  <div class="row">
+<div class="content">
+  <h1 class="page-title">{{ $title }}
+    <span class="pull-right"><a href="{{ route('create_user') }}" class="btn btn-default"><i class="fa fa-user-plus"></i>&nbsp;Add User</a></span>
+  </h1>
+  @if ($users)
+  <table class="table">
+    <caption>Optional table caption.</caption>
+    <thead>
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Username</th>
+        <th>Admin</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
 
-    <div class="content">
-      <h1 class="page-title">{{ $title }}
-        <span class="pull-right"><a href="{{ route('create_user') }}" class="btn btn-default"><i class="fa fa-user-plus"></i>&nbsp;Add User</a></span>
-      </h1>
-      @if ($users)
-      <table class="table">
-        <caption>Optional table caption.</caption>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-            <th>Admin</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
+    @foreach ($users as $user)
+    <tr>
+      <td>{{ $user->first_name }}</td>
+      <td>{{ $user->last_name }}</td>
+      <td>{{ $user->email }}</td>
+      <td>{{ ($user->admin == "1" ? "True" : "&mdash;") }}</td>
+      <td><a href="{{ route('admin') }}/users/{{ $user->id }}">Edit</a> | Disable</td>
+    </tr>
 
-        @foreach ($users as $user)
-        <tr>
-          <td>{{ $user->first_name }}</td>
-          <td>{{ $user->last_name }}</td>
-          <td>{{ $user->email }}</td>
-          <td>{{ ($user->admin == "1" ? "True" : "&mdash;") }}</td>
-          <td><a href="{{ route('admin') }}/users/{{ $user->id }}">Edit</a> | Disable</td>
-        </tr>
+    @endforeach
+    </tbody>
+  </table>
 
-        @endforeach
-        </tbody>
-      </table>
+  @else
+  <h3>No Users</h3>
+  @endif
+</div><!-- /.content -->
 
-      @else
-      <h3>No Users</h3>
-      @endif
-    </div><!-- /.content -->
-  </div><!-- /.row -->
-
-</div><!-- /.container -->
 @stop
