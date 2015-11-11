14c0cb26fce8f3eb8c01a06fda10aa4a515fe1b1
Derek Nutile <dereknutile@gmail.com>
2015-11-11 07:52:12 -0800

    Pulls the container wrapper out of each sub view.  Also changes the container to a container-fluid.

--- core/resources/views/_layouts/admin.blade.php
+++ core/resources/views/_layouts/admin.blade.php
@@ -12,9 +12,17 @@
       <div class="col-sm-3 col-md-2 sidebar admin-sidebar">
       @include('_includes/admin_navigation')
       </div>
+
       <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-      @yield('content')
+        <div class="container-fluid">
+          <div class="row">
+
+            @yield('content')
+
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
       </div>
+
     </div>
   </div>
 
