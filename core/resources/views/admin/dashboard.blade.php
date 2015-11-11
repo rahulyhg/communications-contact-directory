b50a3879135f39d651fa0c6040f46787039a0e33
Derek Nutile <dereknutile@gmail.com>
2015-11-11 07:54:02 -0800

    Removes container wrapper from remaining admin subviews.

--- core/resources/views/admin/dashboard.blade.php
+++ core/resources/views/admin/dashboard.blade.php
@@ -1,13 +1,9 @@
 @extends('_layouts/admin')
 
 @section('content')
-<div class="container">
-  <div class="row">
-    <div class="content">
 
-    <h1 class="page-title">{{ $title }}</h1>
+<div class="content">
+<h1 class="page-title">{{ $title }}</h1>
+</div><!-- /.content -->
 
-    </div><!-- /.content -->
-  </div><!-- /.row -->
-</div><!-- /.container -->
 @stop
