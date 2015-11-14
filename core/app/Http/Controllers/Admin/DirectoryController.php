abe69415d161c0e337d9dcdc8f674d9f46262149
Derek Nutile <dereknutile@gmail.com>
2015-11-13 16:03:58 -0800

    Implements a directory index table.

--- core/app/Http/Controllers/Admin/DirectoryController.php
+++ core/app/Http/Controllers/Admin/DirectoryController.php
@@ -4,86 +4,89 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 
+use App\Directory;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class DirectoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Response
-     */
-    public function index()
-    {
-      $title = "Directory Management";
-      $page_active = "directory";
-      return view('admin.directory.index', compact('title','page_active'));
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return Response
+   */
+  public function index()
+  {
+    $title = "Directory Management";
+    $page_active = "directory";
+    $entries = Directory::all();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
+    return view('admin.directory.index', compact('title','page_active', 'entries'));
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  Request  $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return Response
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  Request  $request
+   * @return Response
+   */
+  public function store(Request $request)
+  {
+    //
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function show($id)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  Request  $request
-     * @param  int  $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function edit($id)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  Request  $request
+   * @param  int  $id
+   * @return Response
+   */
+  public function update(Request $request, $id)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
 }
--- core/resources/views/admin/directory/index.blade.php
+++ core/resources/views/admin/directory/index.blade.php
@@ -3,7 +3,41 @@
 @section('content')
 
 <div class="content">
-<h1 class="page-title">{{ $title }}</h1>
+  <h1 class="page-title">{{ $title }}
+    <span class="pull-right"><a href="{{ route('admin.directory.create') }}" class="btn btn-default"><i class="fa fa-user-plus"></i>&nbsp;Add User</a></span>
+  </h1>
+  @if ($entries)
+  <table class="table">
+    <caption>{{ count($entries) }} Directory Entries</caption>
+    <thead>
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Location</th>
+        <th>Department</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+
+    @foreach ($entries as $entry)
+    <tr>
+      <td>{{ $entry->first_name }}</td>
+      <td>{{ $entry->last_name }}</td>
+      <td>{{ $entry->email }}</td>
+      <td>{{ $entry->location }}</td>
+      <td>{{ $entry->department }}</td>
+      <td><a href="{{ route('admin.directory.edit',$entry->id) }}">edit</a> | {{ ($entry->status == "0" ? "enable" : "disable") }}</td>
+    </tr>
+
+    @endforeach
+    </tbody>
+  </table>
+
+  @else
+  <h3>No Users</h3>
+  @endif
 </div><!-- /.content -->
 
 @stop
