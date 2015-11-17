3a6157244da9bde876fe2bca00ebf4b8e9d0774c
Derek Nutile <dereknutile@gmail.com>
2015-11-16 16:43:01 -0800

    Creates models for location and deparment.

---
+++ core/app/Department.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+  /**
+   * The database table used by the model.
+   *
+   * @var string
+   */
+  protected $table = 'department';
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+    // protected $fillable = ['item', 'item',];
+
+  /**
+   * The attributes excluded from the model's JSON form.
+   *
+   * @var array
+   */
+  // protected $hidden = ['item', 'item',];
+}
+}
