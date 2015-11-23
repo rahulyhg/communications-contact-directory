b6b9873ce0d688f437681caee18685dad2be3d10
Derek Nutile <dereknutile@gmail.com>
2015-11-23 15:23:09 -0800

    Builds contact form.

--- core/resources/views/pages/contact.blade.php
+++ core/resources/views/pages/contact.blade.php
@@ -2,20 +2,42 @@
 
 @section('content')
 <div class="container">
-	<div class="row">
-		<div class="sign-in-form">
-			<div class="well">
-				<h3>Contact Us</h3>
+  <div class="row">
+    <div class="sign-in-form">
+      <div class="well">
+        <h3>Contact Us</h3>
 
         {!! Form::open() !!}
-					<div class="form-group">
-					{!! Form::label('name','Name') !!}
-					{!! Form::text('name', null, array('class'=>'form-control','placeholder'=>'Name')) !!}
-					</div><!-- /.form-group -->
+
+          <div class="form-group">
+            {!! Form::label('name', 'Name', ['class'=>'required']) !!}
+            {!! Form::text('name', null, ['required', 'class'=>'form-control', 'placeholder'=>'First Last']) !!}
+          </div>
+
+          <div class="form-group">
+            {!! Form::label('email', 'Email', ['class'=>'required']) !!}
+            {!! Form::text('email', null, ['required', 'class'=>'form-control', 'placeholder'=>'email@domain.com']) !!}
+          </div>
+
+          <div class="form-group">
+            {!! Form::label('subject', 'Subject', ['class'=>'required']) !!}
+            {!! Form::select('subject', ['General message'=>'General message','Name correction, addition, or removal'=>'Name correction, addition, or removal'], null, ['required', 'class'=>'form-control']) !!}
+          </div>
+
+          <div class="form-group">
+            {!! Form::label('message', 'Message', ['class'=>'required']) !!}
+            {!! Form::textarea('message', null, ['required', 'class'=>'form-control', 'placeholder'=>'Your message here.']) !!}
+          </div>
+
+          <div class="form-group">
+            {!! Form::submit('Submit', ['class'=>'btn btn-primary']) !!}
+            <a href="{{ route('home') }}" class="btn btn-default">Cancel</a>
+          </div>
+
         {!! Form::close() !!}
 
-			</div><!-- /.well -->
-		</div><!-- /.sign-in-form -->
-	</div><!-- /.row -->
+      </div><!-- /.well -->
+    </div><!-- /.sign-in-form -->
+  </div><!-- /.row -->
 </div><!-- /.container -->
 @stop
