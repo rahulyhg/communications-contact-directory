6fff2843c468179c9d719c4549b06a04f7c09dba
Derek Nutile <dereknutile@gmail.com>
2015-11-25 08:39:17 -0800

    Adds an action to the hide method of the entry models to clear content.

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -3,8 +3,14 @@ $(document).ready(function(){
 
   $('.alert-fade-away').delay(3000).slideUp(300);
 
+  // populate the model with the current entry information on the show method
   $("#entryModal").on("show.bs.modal", function(e) {
-      var link = $(e.relatedTarget);
-      $(this).find(".modal-content").load(link.attr("href"));
+    var link = $(e.relatedTarget);
+    $(this).find(".modal-content").load(link.attr("href"));
+  });
+
+  // clear the model on the hide method
+  $("#entryModal").on("hide.bs.modal", function(e) {
+    $(this).find(".modal-content").empty();
   });
 });
