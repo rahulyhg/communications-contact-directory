2feb4090ccf9f6c83de02943d7d61121cedaed8d
Derek Nutile <dereknutile@gmail.com>
2015-12-03 14:44:23 -0800

    Blurs background images and updates footer styling.

--- core/resources/views/_includes/footer.blade.php
+++ core/resources/views/_includes/footer.blade.php
@@ -5,17 +5,17 @@
       <div class="col-md-6">
         <h3>Links</h3>
           <ul class="list-unstyled">
-            <li><a href="#">Washington County Web Site</a></li>
-            <li><a href="#">LUT Website</a></li>
+            <li><a href="http://www.co.washington.or.us/" target="_blank"><i class="fa fa-external-link fa-pad-right"></i>Washington County Web Site</a></li>
+            <li><a href="http://www.co.washington.or.us/lut" target="_blank"><i class="fa fa-external-link fa-pad-right"></i>LUT Website</a></li>
           </ul>
       </div><!-- /.col-md-6 -->
 
       <div class="col-md-6">
         <h3>Contact Us!</h3>
         <ul class="horizontal-list social-list">
-          <li><a href="#"><i class="fa fa-3x fa-twitter"></i>&nbsp;</a></li>
-          <li><a href="#"><i class="fa fa-3x fa-facebook"></i>&nbsp;</a></li>
-          <li><a href="#"><i class="fa fa-3x fa-instagram"></i>&nbsp;</a></li>
+          <li><a href="#" target="_blank" class="fa-hover"><i class="fa fa-3x fa-twitter"></i></a></li>
+          <li><a href="#" target="_blank" class="fa-hover"><i class="fa fa-3x fa-facebook"></i></a></li>
+          <li><a href="#" target="_blank" class="fa-hover"><i class="fa fa-3x fa-instagram"></i></a></li>
         </ul>
       </div><!-- /.col-md-6 -->
     </div><!-- /.container -->
--- core/resources/views/_layouts/landing.blade.php
+++ core/resources/views/_layouts/landing.blade.php
@@ -23,7 +23,7 @@
         </div><!-- /.wrapper -->
     </div><!-- /.page-content -->
 
-    <div id="bg"><img src="/assets/img/mount-hood.jpg" alt=""></div>
+    <div id="bg"><img src="/assets/img/mount-hood-blurred.jpg" alt=""></div>
 @include('_includes/scripts')
 @include('_includes/analytics')
 
