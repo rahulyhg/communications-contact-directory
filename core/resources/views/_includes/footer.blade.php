4ab4f8edd16abf33be0318d771623c9f0abc49e4
Derek Nutile <dereknutile@gmail.com>
2015-12-03 11:39:28 -0800

    Updates footer design.

--- core/resources/views/_includes/footer.blade.php
+++ core/resources/views/_includes/footer.blade.php
@@ -2,30 +2,32 @@
 <footer>
   <section class="footer-section">
     <div class="container">
-      <div class="col-md-4">
-        <h3><i class="fa fa-comments-o"></i>&nbsp;Contact Us!</h3>
-        <address>
-          <strong>Address</strong>
-        </address>
-      </div><!-- /.col-md-4 -->
-
-      <div class="col-md-4">
-        <h3><i class="fa fa-external-link"></i>&nbsp;Links</h3>
+      <div class="col-md-6">
+        <h3>Links</h3>
           <ul class="list-unstyled">
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
+            <li><a href="#">Washington County Web Site</a></li>
+            <li><a href="#">LUT Website</a></li>
           </ul>
-      </div><!-- /.col-md-4 -->
+      </div><!-- /.col-md-6 -->
 
-      <div class="col-md-4">
-        <h3><i class="fa fa-newspaper-o"></i>&nbsp;Stay Informed</h3>
-          <p>Sign up for our newsletter and be in the know!</p>
-          <div class="input-group">
-          <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-          <input type="email" autocapitalize="off" autocorrect="off" class="form-control input-small" value="" placeholder="your@email.com" required="">
-          </div>
-      </div><!-- /.col-md-4 -->
+      <div class="col-md-6">
+        <h3>Contact Us!</h3>
+        <ul class="horizontal-list social-list">
+          <li><a href="#"><i class="fa fa-3x fa-twitter"></i>&nbsp;</a></li>
+          <li><a href="#"><i class="fa fa-3x fa-facebook"></i>&nbsp;</a></li>
+          <li><a href="#"><i class="fa fa-3x fa-instagram"></i>&nbsp;</a></li>
+        </ul>
+      </div><!-- /.col-md-6 -->
     </div><!-- /.container -->
   </section><!-- /.footer-section -->
+  <section class="footer-usage-section">
+    <div class="container">
+      <p class="usage">Use of this site constitutes acceptance of our user agreement (effective january 1, 2014) and privacy policy (effective january 1, 2014).  The material on this site may not be reproduced, distributed, transmitted, cached or otherwise used, except with the prior written permission of Washington County.</p>
+    </div><!-- /.container -->
+  </section><!-- /.footer-usage-section -->
+  <section class="footer-copyright-section">
+    <div class="container">
+      <p class="copyright text-center">Copyright &copy; {{ date('Y') }}</p>
+    </div><!-- /.container -->
+  </section><!-- /.footer-copyright-section -->
 </footer>
