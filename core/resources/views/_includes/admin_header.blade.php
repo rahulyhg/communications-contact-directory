df05f78b2f9dc239a7728badaad97b274bdf0ca1
Derek Nutile <dereknutile@gmail.com>
2015-12-15 14:14:58 -0800

    Updates admin styling.

--- core/resources/views/_includes/admin_header.blade.php
+++ core/resources/views/_includes/admin_header.blade.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="{{ route('admin') }}"><i class="fa fa-home"></i>&nbsp;<span class="hidden-xs">{{ Config::get('app.site_title') }}</span> Admin Panel</a>
+      <a class="navbar-brand navbar-brand-admin" href="{{ route('admin') }}"><i class="fa fa-home"></i>&nbsp;<span class="hidden-xs">{{ Config::get('app.site_title') }}</span> Admin Panel</a>
     </div>
     <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav navbar-right">
