c7da3e85447509614007e16802ec0fbba865c535
Derek Nutile <dereknutile@gmail.com>
2016-02-03 10:02:40 -0800

    Reorganizes the order in which select2 and parsley are called to fix an issue.

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,26 +1,28 @@
 $(document).ready(function(){
-  $('.data-table').dataTable();
-  $('.validate-form').parsley();
-  $('.wysiwyg').summernote({
-    height: 300,                 // set editor height
-    minHeight: null,             // set minimum height of editor
-    maxHeight: null,             // set maximum height of editor
-  });
 
-  $('.alert-fade-away').delay(3000).slideUp(300);
+    // enhance specific dropdowns using Select2
+    // Note: this has to come before the validation (parsley.js)
+    $(".select2_list").select2();
 
-  // populate the model with the current entry information on the show method
-  $("#entryModal").on("show.bs.modal", function(e) {
-    var link = $(e.relatedTarget);
-    $(this).find(".modal-content").load(link.attr("href"));
-  });
+    $('.data-table').dataTable();
+    $('.validate-form').parsley();
 
-  // clear the model on the hide method
-  $("#entryModal").on("hide.bs.modal", function(e) {
-    $(this).find(".modal-content").empty();
-  });
+    $('.wysiwyg').summernote({
+        height: 300,                 // set editor height
+        minHeight: null,             // set minimum height of editor
+        maxHeight: null,             // set maximum height of editor
+    });
 
-  // enhance specific dropdowns using Select2
-  $(".select2_list").select2();
+    $('.alert-fade-away').delay(3000).slideUp(300);
 
+    // populate the model with the current entry information on the show method
+    $("#entryModal").on("show.bs.modal", function(e) {
+        var link = $(e.relatedTarget);
+        $(this).find(".modal-content").load(link.attr("href"));
+    });
+
+    // clear the model on the hide method
+    $("#entryModal").on("hide.bs.modal", function(e) {
+        $(this).find(".modal-content").empty();
+    });
 });
