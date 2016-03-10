e2bf54c860cf426711e3e443e5b185559b8ccaf0
Derek Nutile <dereknutile@gmail.com>
2016-03-10 15:28:08 -0800

    Updates footer to use common styling.

--- core/resources/views/_includes/footer.blade.php
+++ core/resources/views/_includes/footer.blade.php
@@ -1,33 +1,32 @@
 
-<footer>
-  <section class="footer-section">
-    <div class="container">
-      <div class="col-md-6">
-        <h3>Links</h3>
-          <ul class="list-unstyled">
-            <li><a href="http://www.co.washington.or.us/" target="_blank"><i class="fa fa-external-link fa-pad-right"></i>Washington County Web Site</a></li>
-            <li><a href="http://www.co.washington.or.us/lut" target="_blank"><i class="fa fa-external-link fa-pad-right"></i>LUT Website</a></li>
-          </ul>
-      </div><!-- /.col-md-6 -->
 
-      <div class="col-md-6">
-        <h3>Contact Us!</h3>
-        <ul class="horizontal-list social-list">
-          <li><a href="#" target="_blank" class="fa-hover"><i class="fa fa-3x fa-twitter"></i></a></li>
-          <li><a href="#" target="_blank" class="fa-hover"><i class="fa fa-3x fa-facebook"></i></a></li>
-          <li><a href="#" target="_blank" class="fa-hover"><i class="fa fa-3x fa-instagram"></i></a></li>
-        </ul>
-      </div><!-- /.col-md-6 -->
-    </div><!-- /.container -->
-  </section><!-- /.footer-section -->
-  <section class="footer-usage-section">
+
+<footer class="footer">
     <div class="container">
-      <p class="usage">Use of this site constitutes acceptance of our user agreement (effective january 1, 2014) and privacy policy (effective january 1, 2014).  The material on this site may not be reproduced, distributed, transmitted, cached or otherwise used, except with the prior written permission of Washington County.</p>
-    </div><!-- /.container -->
-  </section><!-- /.footer-usage-section -->
-  <section class="footer-copyright-section">
-    <div class="container">
-      <p class="copyright text-center">Copyright &copy; {{ date('Y') }}, Washington County, Oregon</p>
-    </div><!-- /.container -->
-  </section><!-- /.footer-copyright-section -->
+        <div class="row">
+            <div class="col-md-6">
+                <h4>Links</h4>
+                <ul class="list-unstyled">
+                    <li><a href="http://www.co.washington.or.us/" target="_blank"><i class="fa fa-external-link fa-pad-right"></i>Washington County Web Site</a></li>
+                    <li><a href="http://www.co.washington.or.us/lut" target="_blank"><i class="fa fa-external-link fa-pad-right"></i>LUT Website</a></li>
+                </ul>
+            </div>
+            <div class="col-md-6">
+                <h4>Address</h4>
+                <address>
+                    <h5>Washington County Public Services Building</h5>
+                    155 N First Ave<br>
+                    Hillsboro, OR 97124<br>
+                </address>
+            </div>
+        </div><!-- /.row -->
+
+        <div class="row">
+            <p class="usage">Use of this site constitutes acceptance of our user agreement (effective january 1, 2014) and privacy policy (effective january 1, 2014).  The material on this site may not be reproduced, distributed, transmitted, cached or otherwise used, except with the prior written permission of Washington County.</p>
+        </div>
+
+        <div class="row">
+            <p class="copyright">Copyright © {{ date('Y') }} Washington County Oregon</p>
+        </div><!-- /.row -->
+    </div>
 </footer>
