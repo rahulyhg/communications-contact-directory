4713bd816ebadddb2e609e8a13ae090613bbbdd8
Derek Nutile <dereknutile@gmail.com>
2016-03-10 15:27:22 -0800

    Removes the local custom css and points the application to the CDN.

--- core/resources/views/_includes/head.blade.php
+++ core/resources/views/_includes/head.blade.php
@@ -10,7 +10,9 @@
 <link href="{{ asset('assets/vendor/fontawesome/css/font-awesome.min.css') }}" rel="stylesheet">
 <link href="{{ asset('assets/vendor/summernote/dist/summernote.css') }}" rel="stylesheet">
 <link href="{{ asset('assets/vendor/select2/dist/css/select2.min.css') }}" rel="stylesheet">
-<link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet">
+<!-- <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet"> -->
+<link href="https://s3.amazonaws.com/washcomultimedia/web/css/custom.min.css" rel="stylesheet">
+
 
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
