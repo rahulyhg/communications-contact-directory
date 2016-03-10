c7330a1a1e436c6d05319f075009290d918da6e7
Derek Nutile <dereknutile@gmail.com>
2016-03-10 15:27:53 -0800

    Updates the main navigation to reflect the common style.

--- core/resources/views/_includes/header.blade.php
+++ core/resources/views/_includes/header.blade.php
@@ -1,28 +1,37 @@
 <header>
-  <nav class="navbar navbar-static-top navbar-public">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="{{ route('home') }}"><img src="{{ asset('assets/img/logo.png') }}" class="brand-logo" /></a>
-      </div>
-    </div>
-  </nav>
+    <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand text-shadow-dark" href="{{ route('home') }}">{{ Config::get('app.site_title') }}</a>
+            </div>
+            @include('_includes/sub_navigation')
+        </div>
+    </nav>
 
-  @include('_includes/sub_navigation')
-  @include('_includes/flash')
+    @include('_includes/flash')
 
-  <section class="header-section">
-    <div class="container pre-post-padding">
-      <h1 class="page-title">{{ Config::get('app.site_title') }}</h1>
-      @if (isset($title))
-      <h2 class="page-subtitle">{{ $title }}</small></h2>
-      @endif
-    </div><!-- /.container -->
+    <section class="header-section">
+        <div class="container pre-post-padding">
+            <div class="row">
+                <div class="col-sm-3">
+                    <a class="navbar-brand" href="{{ route('home') }}">
+                        <img src="https://s3.amazonaws.com/washcomultimedia/web/img/logo.png" class="img-responsive img-center img-padded">
+                    </a>
+                </div>
+                <div class="col-sm-9">
+                    <h1 class="page-title">{{ Config::get('app.site_title') }}</h1>
+                    @if (isset($title))
+                    <h2 class="page-subtitle">{{ $title }}</small></h2>
+                    @endif
+                </div>
+            </div>
+        </div><!-- /.container -->
 
-  </section><!-- /.header-section -->
+    </section><!-- /.header-section -->
 </header>
